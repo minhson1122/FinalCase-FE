3efb0f516a4219a2fc25595017c639e71003556e
MinhHieu3 <hieubyi@gmail.com>
2024-02-26 21:18:44 +0700

    done hiển thị playlist

--- js/login.js
+++ js/login.js
@@ -33,13 +33,12 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
         console.log(res.data)
         console.log(res.data.accessToken)
         console.log(res.data.roles[0].authority)
-       if (res.data.roles[0].authority==='ROLE_USER'){
-           loginUser()
+        if (res.data.roles[0].authority === 'ROLE_USER') {
+            loginUser()
 
-       }
-       else if(res.data.roles[0].authority==='ROLE_ADMIN'){
-           alert("tk admin")
-       }
+        } else if (res.data.roles[0].authority === 'ROLE_ADMIN') {
+            alert("tk admin")
+        }
     })
         .catch(error => {
             console.error(error);
@@ -48,15 +47,34 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
             }
         })
 });
+
 function loginUser() {
     const token = localStorage.getItem('userToken');
+    const nameItem = document.getElementById("name-item")
+    const imgItem = document.getElementById("img-item")
+    const playList = document.getElementById("playlist-list")
     if (token !== null) {
         axios.get('http://localhost:8080/api/playLists', {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
         }).then(res => {
-            console.log("data", res)
+            const playlistList = document.getElementById("playlist-list");
+            playlistList.innerHTML = '';
+            res.data.forEach((item) => {
+                const itemDiv = document.createElement("div");
+                itemDiv.classList.add("item-list");
+                const img = document.createElement("img");
+                img.setAttribute("src", item.avatar);
+                img.setAttribute("alt", "Playlist image");
+                const name = document.createElement("p");
+                name.classList.add("name-item");
+                name.textContent = item.name;
+                itemDiv.appendChild(img);
+                itemDiv.appendChild(name);
+                playlistList.appendChild(itemDiv);
+            })
+
             newBackground.style.display = "none";
             homeUser.style.display = "block";
             forUser.style.display = "flex"
@@ -66,9 +84,21 @@ function loginUser() {
             profileNav.style.display = "flex";
         })
 
+        playList.addEventListener('click',function (){
+            axios.get('http://localhost:8080/api/song-playlist', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            }).then(res => {
+                console.log("play",res.data)
+                console.log("song",res.data)
+            });
+
+        })
     }
 
 }
+
 document.getElementById("xLogin-btn").addEventListener("click", function () {
     newBackground.style.display = "none";
     home.style.opacity = "100%";
