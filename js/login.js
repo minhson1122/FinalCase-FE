3a167ce8d58af612ce3f55ce2b653d5a08bfcb0c
MinhHieu3 <hieubyi@gmail.com>
2024-02-26 09:49:14 +0700

    done đăng nhập đăng xuất

--- js/login.js
+++ js/login.js
@@ -1,63 +1,85 @@
-var newBackground = document.getElementById("new-background");
-var homeUser = document.getElementById("home")
-var signup = document.getElementById("signup-background")
+let newBackground = document.getElementById("new-background");
+let homeUser = document.getElementById("home")
+let signup = document.getElementById("signup-background")
 let forUser = document.getElementById("for-user")
 let forUser1 = document.getElementById("for-user1")
 let loginNav = document.getElementById("login-nav")
 let profileNav = document.getElementById("profile-nav")
 
-document.getElementById("myButton").addEventListener("click", function() {
-    newBackground.style.display="block";
-    home.style.opacity="75%";
+document.getElementById("myButton").addEventListener("click", function () {
+    newBackground.style.display = "block";
+    home.style.opacity = "75%";
 });
-document.getElementById("login-box").addEventListener("click", function() {
-    newBackground.style.display="block";
-    signup.style.display="none";
-    home.style.opacity="75%";
+document.getElementById("login-box").addEventListener("click", function () {
+    newBackground.style.display = "block";
+    signup.style.display = "none";
+    home.style.opacity = "75%";
 });
-document.getElementById("signup-box").addEventListener("click", function() {
-    signup.style.display="block";
-    newBackground.style.display="none";
-    home.style.opacity="75%";
+document.getElementById("signup-box").addEventListener("click", function () {
+    signup.style.display = "block";
+    newBackground.style.display = "none";
+    home.style.opacity = "75%";
 });
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('loginForm').addEventListener('submit', function (event) {
     event.preventDefault();
     var username = document.querySelector('[name="username"]').value;
     var password = document.querySelector('[name="password"]').value;
-    data={
+    data = {
         "username": username,
         "password": password
     }
-    axios.post(`http://localhost:8080/login`,data).then(res=>{
-        console.log(res)
-        newBackground.style.display = "none";
-        homeUser.style.display = "block";
-        forUser.style.display="flex"
-        forUser1.style.display="block"
-        newBackground.style.display="none";
-        home.style.opacity="100%";
-        loginNav.style.display="none";
-        profileNav.style.display="flex";
-
+    axios.post(`http://localhost:8080/login`, data).then(res => {
+        localStorage.setItem('userToken', res.data.accessToken);
+        console.log(res.data)
+        console.log(res.data.accessToken)
+        window.location.reload()
     })
         .catch(error => {
             console.error(error);
             if (error.response && error.response.status === 401) {
                 alert("Tài Khoản Hoặc Mật Khẩu Sai");
             }
+        })
+});
+const token = localStorage.getItem('userToken');
+if (token !== null) {
+    axios.get('http://localhost:8080/api/playLists', {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    }).then(res => {
+        console.log("data", res)
+        newBackground.style.display = "none";
+        homeUser.style.display = "block";
+        forUser.style.display = "flex"
+        forUser1.style.display = "block"
+        home.style.opacity = "100%";
+        loginNav.style.display = "none";
+        profileNav.style.display = "flex";
     })
+
+}
+
+document.getElementById("xLogin-btn").addEventListener("click", function () {
+    newBackground.style.display = "none";
+    home.style.opacity = "100%";
 });
-document.getElementById("xLogin-btn").addEventListener("click", function() {
-    newBackground.style.display="none";
-    home.style.opacity="100%";
-});
-document.getElementById("xSignup-btn").addEventListener("click", function() {
-    signup.style.display="none";
-    home.style.opacity="100%";
+document.getElementById("xSignup-btn").addEventListener("click", function () {
+    signup.style.display = "none";
+    home.style.opacity = "100%";
 });
 
 document.getElementById("main-view").addEventListener("click", function () {
-    newBackground.style.display="none";
-    signup.style.display="none";
-    home.style.opacity="100%";
+    newBackground.style.display = "none";
+    signup.style.display = "none";
+    home.style.opacity = "100%";
 });
+document.getElementById("logout").addEventListener("click", function () {
+    localStorage.setItem('userToken', null);
+    console.log(localStorage.getItem('userToken'))
+    forUser1.style.display = "none"
+    newBackground.style.display = "none";
+    home.style.opacity = "100%";
+    loginNav.style.display = "block";
+    profileNav.style.display = "none";
+})
--- js/music.js
+++ js/music.js
@@ -43,42 +43,43 @@ const getImageData = (e) => {
 };
 
 
-
 axios.get('http://localhost:8080/api/songs').then(res => {
     const card = document.getElementById("card");
     const control = document.getElementById("control");
     const love = document.getElementById("love");
     card.innerHTML = '';
     let songCount = 0;
-    let songIndex=-1;
-    let indexs=-1;
-    const songs=res.data
-     function playSong(index) {
-         if (index < 0 || index >= songs.length) return;
-         songIndex = index;
-         const song = songs[index];
-         toggleAudio(song.src,song.name,song.singer.name,song.album.avatar)
-         updateLike(song.likes)
-         axios.get(`http://localhost:8080/api/songs/listen/${song.id}`
-         ).then(res=>{
-             console.log(res.data)
+    let songIndex = -1;
+    let indexs = -1;
+    const songs = res.data
 
-         })
+    function playSong(index) {
+        if (index < 0 || index >= songs.length) return;
+        songIndex = index;
+        const song = songs[index];
+        toggleAudio(song.src, song.name, song.singer.name, song.album.avatar)
+        updateLike(song.likes)
+        axios.get(`http://localhost:8080/api/songs/listen/${song.id}`
+        ).then(res => {
+            console.log(res.data)
+
+        })
 
 
-     }
-    document.getElementById("nextSong").addEventListener("click", function() {
+    }
+
+    document.getElementById("nextSong").addEventListener("click", function () {
         if (songIndex >= 0 && songIndex < songs.length - 1) {
             playSong(songIndex + 1);
 
         }
     });
-    document.getElementById("prevSong").addEventListener("click", function() {
+    document.getElementById("prevSong").addEventListener("click", function () {
         if (songIndex > 0) {
             playSong(songIndex - 1);
         }
     });
-    songs.forEach ((item,index) =>{
+    songs.forEach((item, index) => {
         const songDiv = document.createElement("div");
         songDiv.className = "App__section-grid-item";
         songDiv.innerHTML = `
@@ -86,24 +87,25 @@ axios.get('http://localhost:8080/api/songs').then(res => {
             <div class="song-name">${item.name}</div>
             <span>${item.singer.name}</span>
         `;
-        songDiv.querySelector('.song-name').addEventListener('click', () =>{
+        songDiv.querySelector('.song-name').addEventListener('click', () => {
             playSong(index)
 
-            control.style.display='block';
-            love.style.display='block';
-            indexs=`${item.id}`;
+            control.style.display = 'block';
+            love.style.display = 'block';
+            indexs = `${item.id}`;
         });
         card.appendChild(songDiv);
         songCount++;
 
     });
-    love.addEventListener('click', ()=>{
+    love.addEventListener('click', () => {
         axios.get(`http://localhost:8080/api/songs/like/${indexs}`
-        ).then(res=>{
+        ).then(res => {
             updateLike(res.data.likes)
 
         })
     });
 
 });
+
 
